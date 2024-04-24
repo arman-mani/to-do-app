46dfc945b1dad842a3d030cbc0c9f338c0d6bfec
Arman <arman.karimi@chasacademy.se>
2024-04-24 22:23:52 +0200

    localstorage o conditional för färdig todo

--- src/pages/todos.jsx
+++ src/pages/todos.jsx
@@ -1,13 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './todos.css'; 
 
 const Todos = () => { 
   const [todos, setTodos] = useState([]);
   const [todoInput, setTodoInput] = useState('');
 
+  const addToLocalStorage = (todo) => {
+    const existingTodos = JSON.parse(localStorage.getItem('TODO_LIST')) || [];
+    existingTodos.push(todo);
+    localStorage.setItem('TODO_LIST', JSON.stringify(existingTodos));
+  }
+
+  const getFromLocalStorage = () => {
+    const storedTodos = JSON.parse(localStorage.getItem('TODO_LIST'));
+    return storedTodos || [];
+  }
+
+  useEffect(() => {
+    const storedTodos = getFromLocalStorage();
+    setTodos(storedTodos);
+  }, []);
+
+  const allTodosCompleted = () => {
+    return todos.every(todo => todo.completed);
+  };
+
   const addTodo = () => {
     if (todoInput.trim() !== '') {
-      setTodos([...todos, { text: todoInput, completed: false }]);
+      const newTodo = { text: todoInput, completed: false };
+      setTodos([...todos, newTodo]);
+      addToLocalStorage(newTodo); 
       setTodoInput('');
     }
   };
@@ -16,16 +38,19 @@ const Todos = () => {
     const updatedTodos = [...todos];
     updatedTodos.splice(index, 1);
     setTodos(updatedTodos);
+    localStorage.setItem('TODO_LIST', JSON.stringify(updatedTodos)); 
   };
 
   const toggleComplete = (index) => {
     const updatedTodos = [...todos];
     updatedTodos[index].completed = !updatedTodos[index].completed;
     setTodos(updatedTodos);
+    localStorage.setItem('TODO_LIST', JSON.stringify(updatedTodos)); 
   };
 
   const clearTodos = () => {
     setTodos([]);
+    localStorage.removeItem('TODO_LIST'); 
   };
 
   return (
@@ -56,9 +81,12 @@ const Todos = () => {
         ))}
       </ul>
       <button onClick={clearTodos} className="todo-button">Rensa alla</button>
+      
+      {todos.length > 0 && allTodosCompleted() && (
+        <p>Du är färdig. Bra jobbat!</p>
+      )}
     </div>
   );
 }
 
-export default Todos; 
-
+export default Todos;
